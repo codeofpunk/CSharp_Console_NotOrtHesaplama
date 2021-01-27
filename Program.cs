724b9b47e7b57cba15a26c2e42a7dcdd81c601d4
Batuhan <61819067+codeofpunk@users.noreply.github.com>
2021-01-27 23:07:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_OrtalamaHesap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vize1;
+            double vize2;
+            double final;
+            double ortalama;
+
+            Console.WriteLine("****** Ders Not Ortalaması Hesaplama ******");
+            Console.Write("1. Vize Notunuz:");
+            vize1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("2. Vize Notunuz:");
+            vize2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Final Notunuz:");
+            final = Convert.ToDouble(Console.ReadLine());
+
+            ortalama = (vize1 + vize2) * 0.2 + final * 0.6;
+
+            if (ortalama < 60)
+            {
+                Console.WriteLine(ortalama + " Ortalama ile dersten kaldınız! :(");
+            }
+
+            else
+            {
+                Console.WriteLine(ortalama + " Ortalama ile başarıyla dersi geçtiniz.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
